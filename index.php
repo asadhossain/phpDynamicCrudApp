801526bb08b5007833105282158703c1275dc50b
asadhossain <developerasad007@gmail.com>
2023-03-16 00:16:35 +0600

    asad

--- index.php
+++ index.php
@@ -8,10 +8,14 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>php Dynamic Crud App!</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    container
+
+
+
+
 
     <!-- Optional JavaScript; choose one of the two! -->
 
