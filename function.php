162f68db06661f46330aa23b8fb26d985157860c
asadhossain <developerasad007@gmail.com>
2023-03-17 03:34:23 +0600

    A

--- function.php
+++ function.php
@@ -1,5 +1,21 @@
 <?php
+        class crudApp{
 
+            private $conn;
+
+            public function __construct()
+            #database host, database user, database pass, database name.
+            $dbhost = "localhost";
+            $dbuser = "root";
+            $dbpass = "";
+            $dbname = "phpdynamiccrudapp";
+
+            $this->conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname );
+
+
+
+
+        }
 
 
 
