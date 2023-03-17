873af6dd8f52844456f239ee4dcca12021cf03bc
asadhossain <developerasad007@gmail.com>
2023-03-18 02:13:36 +0600

    asad

--- edit.php
+++ edit.php
@@ -47,11 +47,11 @@
 
         <?php if(isset($return_mgs)) { echo $return_mgs; } ?>
         <form class= "form" action="" method= "POST" enctype="multipart/form-data" >
-            <input class="form-control mb-2" type="text" name="u_std_name" value="<?php echo $returndata['std_name']?>">
+            <input class="form-control mb-2" type="text" name="u_std_name" value="<?php echo $returndata['std_name']; ?>">
 
-            <input class="form-control mb-2" type="email" name="u_std_email" value="<?php echo $returndata['std_emali']?>">
+            <input class="form-control mb-2" type="email" name="u_std_email" value="<?php echo $returndata['std_emali']; ?>">
 
-            <input class="form-control mb-2" type="number" name="u_std_roll" value="<?php echo $returndata['std_roll']?>">
+            <input class="form-control mb-2" type="number" name="u_std_roll" value="<?php echo $returndata['std_roll']; ?>">
 
             <label class="mb-2" for="image">Upload Your Image</label>
 
